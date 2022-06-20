f726da00437d4c2aae168eb1355ba26ce781c8c6
Arthur <arthur@pop-os.localdomain>
2022-06-20 16:53:54 -0300

    YT streaming, only streams a few seconds

--- src/commands/streaming_commands.py
+++ src/commands/streaming_commands.py
@@ -6,3 +6,8 @@ class StreamingCommands(Cog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
+
+    @command(name="play", help="Toca uma música a partir de uma url do youtube")
+    async def play(self, ctx: Context):
+        await play_audio_from_url(ctx, self.bot, "https://www.youtube.com/watch?v=5sMKX22BHeE")
+        return
--- src/utils/play_yt_audio.py
+++ src/utils/play_yt_audio.py
@@ -1,2 +1,23 @@
-async def play_audio_from_url(url: str):
-    pass
+from discord.ext.commands import Context, Bot
+from discord import FFmpegPCMAudio
+from time import sleep
+import pafy
+
+
+async def play_audio_from_url(ctx: Context, bot: Bot, url: str):
+    if ctx.author != bot.user:
+        try:
+            voice_channel = ctx.author.voice
+            if voice_channel is not None:
+                vc = await voice_channel.channel.connect()
+                audio = pafy.new(url)
+                audio_stream = audio.getbestaudio()
+                vc.play(FFmpegPCMAudio(audio_stream.url))
+                while vc.is_playing():
+                    sleep(1)
+                await vc.disconnect()
+            else:
+                await ctx.reply('é necessário estar em um chat de voz para utilizar esse comando')
+        except Exception as e:
+            print(e.args)
+            await vc.disconnect()
